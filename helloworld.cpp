f8639baee8711ce000816321308890e5e4a0976d
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-05-08 02:58:33 +0300

    removed boost

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,4 +1,3 @@
-#include <boost/dll.hpp>
 #include <fmt/core.h>
 #include <crashpad/client/crashpad_client.h>
 
@@ -8,16 +7,6 @@ int main()
 
     crashpad::CrashpadClient client;
     fmt::println("client path: %s", client.RegisterWerModule(L"path"));
-
-    try
-    {
-        boost::dll::shared_library lib("C:\\Windows\\System32\\kernel32.dll");
-        fmt::println(lib.location().string());
-        fmt::println(lib.suffix().string());
-    }
-    catch (...)
-    {
-    }
 
     return 0;
 }
