0122a5619380154a55a56f3f8627fb4990331798
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-05-08 03:03:30 +0300

    removed crashpad

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,12 +1,8 @@
 #include <fmt/core.h>
-#include <crashpad/client/crashpad_client.h>
 
 int main()
 {
     fmt::println("Hello World!");
-
-    crashpad::CrashpadClient client;
-    fmt::println("client path: %s", client.RegisterWerModule(L"path"));
 
     return 0;
 }
